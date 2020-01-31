6910d621f555624754c54569cf12bb5fda209af9
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-01-31 15:11:59 -0600

    Characters rendered -> search working! Finally . . .

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+export default function Characters() {
+  const [data, setData] = useState([]);
 
+  const [search, setSearch] = useState('');
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
-
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(res => {
+        console.log(res.data.results);
+        const results = res.data.results.filter(character =>
+          character.name.toLowerCase().includes(search.toLowerCase())
+        )
+        setData(results);
+      });
+  }, [search]);
+  const changeHandler = event => {
+    setSearch(event.target.value);
+  };
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Ah gee, you should do a search!"
+          onChange={changeHandler}
+          value={search}
+        />
+      </form>
+      <div>
+        {data.map(data => {
+          return (
+            <div className="char-card" key={data.id}>
+              <h2>{data.name}</h2>
+              <ul>
+                <li>Species: {data.species}</li>
+                <li>Status: {data.status}</li>
+                <li>Location: {data.location.name}</li>
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
